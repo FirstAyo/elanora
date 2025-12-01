5a5f0976b9656b97b567c882c00b339d4a520b6d
Festus <techarenaz01@gmail.com>
2025-12-01 00:23:20 -0800

    added scale image animation to the image in the card

--- src/components/sections/CategoryCard.jsx
+++ src/components/sections/CategoryCard.jsx
@@ -3,8 +3,14 @@ import React from "react";
 function CategoryCard({ className, images, imageAlt, title }) {
   return (
     <div className="relative">
-      <img src={images} alt={imageAlt} className={`${className}`} />
-      <button className="absolute bottom-10 left-16 right-16 py-3 font-semibold bg-white/80 shadow-2xl border border-gray-400">{title}</button>
+      <img
+        src={images}
+        alt={imageAlt}
+        className={`${className} transition-transform duration-800 ease-in-out hover:scale-105`}
+      />
+      <button className="absolute bottom-10 left-16 right-16 py-3 font-semibold bg-white/80 shadow-2xl border border-gray-400">
+        {title}
+      </button>
     </div>
   );
 }
